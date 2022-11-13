ea41b6d0f9e48e724d0e97858bde10884097de73
Mehedi Hasan Murad <mehedihasna500422@gamil.com>
2022-11-13 23:35:26 +0600

    all commit added

---
+++ digital-clock/index.js
@@ -0,0 +1,39 @@
+const hourE1 = document.getElementById('hour');
+const minutesE1 = document.getElementById('minutes');
+const secondsE1 = document.getElementById('seconds');
+const ampmE1 = document.getElementById('ampm');
+
+function updateClock(){
+    let h = new Date().getHours();
+    let m = new Date().getMinutes();
+    let s = new Date().getSeconds();
+    let ampm = "AM";
+
+
+    h = h < 10 ? "0" + h : h;
+    m = m < 10 ? "0" + m : m;
+    s = s < 10 ? "0" + s : s;
+
+    
+    if(h <= 12){
+        ampm="AM"
+        
+    }else{
+        ampm="PM"
+        
+    }
+
+
+    hourE1.innerText = h;
+    minutesE1.innerText = m;
+    secondsE1.innerText = s;
+    ampmE1.innerText = ampm;
+    
+    setTimeout(() =>{
+        updateClock()
+    }, 1000);
+};
+
+updateClock();
+
+
